85785c15c28227e50697cbe572cbc3e070784207
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-17 12:43:04 -0700

    add label class and justify content

--- js/app.js
+++ js/app.js
@@ -2,30 +2,37 @@ $( document ).ready(function() {
 	$('.btn').click( function () {
 		// get values from form 
 	    var programType = $('[name="program-type"]').val();
-	    var programTypeSelector = "." + programType;
+	    var programTypeSelector = programType ? "." + programType : '';
+
 	    // unless all then blank
 	    var eduLevel = $('[name="edu-level"]').val();
-	    var eduLevelSelector = "." + eduLevel;
+	    var eduLevelSelector =  eduLevel ? "." + eduLevel : '';
 	    var	combinedSelector = programTypeSelector + eduLevelSelector;
 
+	    if (combinedSelector === "") {
+	    	$('.images img').addClass('active');
+	    } else {
 
-	    	$('.images img').hide();
+	    	$('.images img').removeClass('active');
+	    	console.log(combinedSelector);
 
-	
-	    	console.log("." + programType);
-	    	console.log("." + eduLevel);
-	    
-		if (programType === '.all' && eduLevel != '.all') {
-			eduLevelSelector
-		} else if (eduLevel === '.all' && programType != '.all') {
-			programTypeSelector
+	    	$(combinedSelector).addClass('active');
 		}
+	
+	    
+	    
+		// if (programType === '.all' && eduLevel != '.all') {
+		// 	eduLevelSelector
+		// } else if (eduLevel === '.all' && programType != '.all') {
+		// 	programTypeSelector
+		// }
 
 
 		// if either are all only check the one that's used
 		// both all show everything
 		// combine $('.SP.HS')
-	});
 
-	// $(".HS").switchClass("", "class2", 1000);
+		});
+
+	
 });
